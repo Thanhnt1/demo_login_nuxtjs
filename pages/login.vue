b12446c99ee560bc6ea00dfc73e0fbd697306ee9
thanhnt1 <thanhnt@paxcreation.com>
2019-11-05 11:12:48 +0700

    Update btn login

--- pages/login.vue
+++ pages/login.vue
@@ -46,7 +46,7 @@
                                         </v-btn>
                                     </v-col>
                                     <v-col cols="12" md="4" sm="6">
-                                        <v-btn block color="primary" @click="login" :loading="loading">Login</v-btn>
+                                        <v-btn block color="primary">Login</v-btn>
                                     </v-col>
                                 </v-row>
                             </v-card-actions>
